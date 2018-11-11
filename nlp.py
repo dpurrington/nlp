4c7ef61e63afaa37ad03b732f553e6e75a62bcc7
Dave Purrington <dave.purrington@gmail.com>
2018-11-11 17:30:55 -0500

    Add comments, clean up some stuff

--- nlp.py
+++ nlp.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python
-
+import sys
 import nltk
 import nltk.data
+
+input_file = sys.argv[1]
 tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
 tokenizer._params.abbrev_types.add('dr')
 tokenizer._params.abbrev_types.add('mr')
 
-f = open("transcript.txt", "r")
+f = open(input_file, "r")
 input_text = f.read()
+# replace the name labels, this can be done differently with
+# a differently-formatted transcript
 input_text = input_text.replace("\nRobert", " ")
 input_text = input_text.replace("\nLara", " ")
 input_text = input_text.replace("\n", " ")
 sentences = tokenizer.tokenize(input_text)
 
-# TODO: rejoin where sentence starts with lower case
-
-sfile = open("sentences.txt", "w")
-for s in sentences:
-    sfile.write(s + "\n")
+# sfile = open("sentences.txt", "w")
+# for s in sentences:
+#    sfile.write(s + "\n")
 
 rejoined = []
+# the tokenizer splits on some things it shouldn't. We assume
+# the capitalization is correct, so we go through and rejoin
+# lines that begin with a lower case letter back to the prior
+# line.
 for i, _ in enumerate(sentences):
-    print(rejoined)
-    print(sentences[i])
-    if not sentences[i][0].islower():
+    if i == 0 or not sentences[i][0].islower():
         rejoined.append(sentences[i])
     else:
         rejoined[-1] = rejoined[-1] + sentences[i]
 
+# questions end with a question mark. Exclude inaudible.
 questions = [q for q in rejoined if q.endswith("?")
              and "[inaudible]" not in q]
 
-
-qfile = open("questions.txt", "w")
+# qfile = open("questions.txt", "w")
 for q in questions:
-    qfile.write(q + "\n")
+    print (q + '\n')
+    # qfile.write(q + "\n")
