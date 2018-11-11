c7c1463451f338e361ae8338f994c2bf69f55681
Dave Purrington <dave.purrington@gmail.com>
2018-11-11 14:40:38 -0500

    rejoin split sentences

--- nlp.py
+++ nlp.py
@@ -1,24 +1,8 @@
-from nltk.corpus import gutenberg
+#!/usr/bin/env python
+
 import nltk
-# nltk.download('gutenberg')
-#nltk.download('punkt')
-from pprint import pprint
-from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktTrainer
-from nltk.corpus import gutenberg
 import nltk.data
-sent_detector = nltk.data.load("tokenizers/punkt/english.pickle")
-
-text = ""
-for file_id in gutenberg.fileids():
-    text += gutenberg.raw(file_id)
-
-#trainer = PunktTrainer()
-#trainer.INCLUDE_ALL_COLLOCS = True
-#trainer.train(text)
-
-#tokenizer = PunktSentenceTokenizer(trainer.get_params())
-tokenizer = sent_detector
-# Test the tokenizer on a piece of text
+tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
 tokenizer._params.abbrev_types.add('dr')
 tokenizer._params.abbrev_types.add('mr')
 
@@ -35,7 +19,18 @@ sfile = open("sentences.txt", "w")
 for s in sentences:
     sfile.write(s + "\n")
 
-questions = [q for q in sentences if q.endswith("?") and "[inaudible]" not in q]
+rejoined = []
+for i, _ in enumerate(sentences):
+    print(rejoined)
+    print(sentences[i])
+    if not sentences[i][0].islower():
+        rejoined.append(sentences[i])
+    else:
+        rejoined[-1] = rejoined[-1] + sentences[i]
+
+questions = [q for q in rejoined if q.endswith("?")
+             and "[inaudible]" not in q]
+
 
 qfile = open("questions.txt", "w")
 for q in questions:
